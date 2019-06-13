0a56acab86e0e42a5a1a86c71c95029377db1e88
dcorre <corre@lal.in2p3.fr>
2019-06-13 19:50:58 +0200

    added scripts to run sextractor and psfex

--- gmadet/gmadet.py
+++ gmadet/gmadet.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from sources_det import sources_det
+from crossmatch import crossmatch
+
+# Define path to data
+path_data = '/home/corre/codes/algoCNN/rawdata/OAJ-T80/'
+
+# Run sextractor to find sources in all images
+sources_det(path_data)
+
+# Cross match with catalogs
+#crossmatch('sources_cat/', catalog='PS1')
