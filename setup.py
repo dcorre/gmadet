2c3e25312549ffb74476a55245f581916dec30df
dcorre <corre@lal.in2p3.fr>
2019-10-14 10:31:29 +0200

    Updated required packages in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'numpy', 'astropy', 'matplotlib', 'pandas']
+requirements = ['Click>=6.0', 'numpy', 'astropy', 'matplotlib',
+                'pandas', 'astroquery', 'astroML']
 
 setup_requirements = ['pytest-runner', ]
 
