e3abec4d91d71ed11ae8151e6c2f5688175ebc1d
dcorre <corre@lal.in2p3.fr>
2020-04-06 17:27:37 +0200

    Adding python script to estimate image background

---
+++ gmadet/background.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+from astropy.stats import SigmaClip
+from photutils import Background2D, SExtractorBackground
+from astropy.io import fits
+
+def bkg_estimation(filename, box=(20,20), filter_size=(3,3), outLevel=1):
+
+    imagelist = np.atleast_1d(filename)
+    for ima in imagelist:
+        print ('\nEstimate background in %s'  % ima)
+        root = os.path.splitext(ima)[0]
+
+        hdulist = fits.open(ima)
+        data = hdulist[0].data
+
+        sigma_clip = SigmaClip(sigma=3.)
+        bkg_estimator = SExtractorBackground()
+        bkg = Background2D(data, box, filter_size=filter_size,
+                           sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+
+        # Create image with background substracted
+        hdulist[0].data = data - bkg.background
+        hdulist.writeto(ima, overwrite=True)
+
+        if outLevel == 2:
+            # Create image with 2D background
+            bkg_file = root + '_bkg.fits'
+            hdulist[0].data = bkg.background
+            hdulist.writeto(bkg_file, overwrite=True)
+
+
+
