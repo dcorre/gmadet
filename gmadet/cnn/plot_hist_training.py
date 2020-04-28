ad59a755a04b1fa79941ed895be334d053d350f7
dcorre <corre@lal.in2p3.fr>
2020-04-28 13:36:27 +0200

    minor

--- gmadet/cnn/plot_hist_training.py
+++ gmadet/cnn/plot_hist_training.py
@@ -8,11 +8,11 @@ def hist(modelname, telescope):
 
     path_gmadet = getpath()
 
-    trainedModelDir = path_gmadet + '/cnn/CNN_training/' + telescope + '/'
-    model_name = trainedModelDir + modelname + '.h5'
+    cubeDir = path_gmadet + '/cnn/datacube/' + telescope + '/'
+    model_name = cubeDir + modelname + '.h5'
 
     print("Loading " + model_name + " ...", end='\r', flush=True)
-    data = np.load(model_name, allow_pickle=True)
+    data = np.load(model_name)
     ima = data["cube"]
     lab = keras.utils.to_categorical(data["labels"])
     mag = data["mags"]
@@ -25,13 +25,13 @@ def hist(modelname, telescope):
 
     plt.figure()
     hist_mag = plt.hist(mag,range = (mag_min, mag_max), bins = 20)
-    plt.savefig(trainedModelDir+modelname+'_hist_mag.png')
+    plt.savefig(cubeDir+modelname+'_hist_mag.png')
     plt.figure()
     hist_dmag = plt.hist(dmag,range = (errmag_min,errmag_max), bins = 20, edgecolor = 'red')
-    plt.savefig(trainedModelDir+modelname+'_hist_errmag.png')
+    plt.savefig(cubeDir+modelname+'_hist_errmag.png')
     plt.figure()
     hist2D = plt.hist2d(mag[mag < 18.5],errmag[mag < 18.5],bins = 100)
-    plt.savefig(trainedModelDir+modelname+'_hist2d.png')
+    plt.savefig(cubeDir+modelname+'_hist2d.png')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -41,7 +41,7 @@ if __name__ == "__main__":
                         dest='modelname',
                         required=True,
                         type=str,
-                        help='Name of the trained model')
+                        help='Name of the datacube')
 
     parser.add_argument('--telescope',
                         dest='telescope',
