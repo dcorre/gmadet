ed6e2ab6c9139d1d2583bfd95d12ea262c772077
PAduverne <pa.duverne@laposte.net>
2020-10-23 17:23:32 +0200

    rebase sim.py with current version

--- gmadet/cnn/sim.py
+++ gmadet/cnn/sim.py
@@ -24,14 +24,13 @@ from skimage.feature import register_translation
 from gmadet.utils import rm_p, mkdir_p
 
 
-def sim(datapath, filenames, Ntrans=50, size=48,
-        magrange=[14, 22], gain=None, magzp=30,
-        radec= None, magnitude=None):
+def sim(datapath, telescope, filenames, Ntrans=50, size=48, magrange=[14, 22],
+        gain=1, magzp=30, radec=None, magnitude=None):
     """Insert point sources in real images """
 
     filenames = np.atleast_1d(filenames)
 
-    #simdir = os.path.join(datapath, "simulation")
+    # simdir = os.path.join(datapath, "simulation")
     simdir = datapath
     mkdir_p(simdir)
 
@@ -60,18 +59,19 @@ def sim(datapath, filenames, Ntrans=50, size=48,
             # flush=True),
             hdusi1 = fits.open(filename, memmap=False)
             headi1 = hdusi1[0].header
-            band = str(headi1['FILTER'])
+            band = str(headi1["FILTER"])
             ima1 = hdusi1[0].data.astype(np.float32)
-            hdusp1 = fits.open(filename.split('.')[0] + '_psf.fits',
-                               memmap=False)
+            hdusp1 = fits.open(
+                os.path.splitext(filename)[0] +
+                "_psf.fits",
+                memmap=False)
             headp1 = hdusp1[0].header
-            step1 = headp1['PSF_SAMP']
-            nb_psf_snaps = int(headp1['PSF_NB'])
+            step1 = headp1["PSF_SAMP"]
+            nb_psf_snaps = int(headp1["PSF_NB"])
             psfs1 = hdusp1[0].data.astype(np.float32)
             imsize = ima1.shape
             posfac = np.array([nb_psf_snaps, nb_psf_snaps]) / imsize
             w = wcs.WCS(headi1)
-
             # try to use GAIN from header
             if gain is None:
                 try:
@@ -90,89 +90,100 @@ def sim(datapath, filenames, Ntrans=50, size=48,
                 filelist.append(os.path.abspath(newfile))
 
                 filterlist.append(band)
-                
                 if radec is None:
-                    pos[j] = np.random.random_sample(2) * (imsize - cutsize) + cutsize / 2.0
+                    pos[j] = np.random.random_sample(
+                        2) * (imsize - cutsize) + cutsize / 2.0
                     # store positions
                     ra, dec = w.wcs_pix2world(pos[j][1], pos[j][0], 1)
                     trans_pix.append(pos[j])
                     trans_wcs.append([ra, dec])
-     
+
                     # get pixels indexes in the image
                     ipos = pos[j].astype(int)
-                    # extract subimage centered on position of the object and store in cima1
+                    # extract subimage centered on position of the object and
+                    # store in cima1
                     # same for weight maps
-                    iposrange = np.s_[ipos[1] - hcutsize[1] : ipos[1] + hcutsize[1], \
-                                      ipos[0] - hcutsize[0] : ipos[0] + hcutsize[0]]
-
+                    iposrange = np.s_[
+                        ipos[1] - hcutsize[1]: ipos[1] + hcutsize[1],
+                        ipos[0] - hcutsize[0]: ipos[0] + hcutsize[0],
+                    ]
                 else:
                     ra, dec = radec[j][0], radec[j][1]
+                    # print('ra, dec = ', ra, dec)
                     pos[j] = w.all_world2pix(ra, dec, 0)
+                    # print(pos[j])
                     trans_pix.append(pos[j])
                     trans_wcs.append([ra, dec])
                     # get pixels indexes in the image
                     ipos = pos[j].astype(int)
                     # print(ipos)
-                    # extract subimage centered on position of the object and store in cima1
+                    # extract subimage centered on position of the object
+                    # and store in cima1
                     # same for weight maps
-                    iposrange = np.s_[ipos[1] - hcutsize[1] : ipos[1] + hcutsize[1], \
-                                      ipos[0] - hcutsize[0] : ipos[0] + hcutsize[0]]
-                    # print(iposrange)
-                # Select the PSF corresponding to the image area, in case there
-                # are more than one PSF estimated per axis
+                    iposrange = np.s_[
+                        ipos[1] - hcutsize[1]: ipos[1] + hcutsize[1],
+                        ipos[0] - hcutsize[0]: ipos[0] + hcutsize[0]]
+                # Select the PSF corresponding to the image area,
+                # in case there are more than one PSF estimated per axis
                 if nb_psf_snaps == 1:
                     psf1 = psfs1
                 else:
-                    # get position with respect to number of PSF snapshots
+                    #  get position with respect to number of PSF snapshots
                     ppos = (pos[j] * posfac).astype(int)
                     psf1 = psfs1[ppos[0], ppos[1]]
-                # step1 is psf_samp parameter from psfex, used in mat1 and 2 to rescale psf to image
+                # step1 is psf_samp parameter from psfex, used in mat1 and 2
+                # to rescale psf to image.
                 # psf1 is from psfex, and has different sampling than image1.
-                # New object put in the center of this matrix
-                mat1 = np.array([[step1, 0.0, hcutsize[0] - psf1.shape[0]*step1 / 2.0], \
-                                 [0.0, step1, hcutsize[1] - psf1.shape[0]*step1 / 2.0]])
+                # New object put in the center of this matrix.
+                mat1 = np.array(
+                    [
+                        [step1, 0., hcutsize[0] - psf1.shape[0] * step1 / 2.],
+                        [0., step1, hcutsize[1] - psf1.shape[0] * step1 / 2.],
+                    ]
+                )
+                # transformation of the PSF, resampling.
+                cpsf1 = cv2.warpAffine(
+                    psf1, mat1, cpsf1.shape, flags=cv2.INTER_LANCZOS4
+                )
 
-                # transformation of the PSF, resampling. 
-                cpsf1 = cv2.warpAffine(psf1, mat1, cpsf1.shape, flags=cv2.INTER_LANCZOS4)
-
-
+                # define the object magnitude using this random number and
+                # predefined ranges
                 if magnitude is None:
-                # define the object magnitude using this random number and predefined ranges
-                    mag = np.random.uniform(low=magrange[0], high=magrange[1], size=(1,))
-                # convert the magnitude in ADU using the zeropoint magnitude.
-                # Note that the zeropoint magnitude is define as 30, so did not care of the exact value
-                # simply needed to draw random magnitudes. We could estimate the proper one for our telescopes
+                    mag = np.random.uniform(
+                        low=magrange[0], high=magrange[1], size=(1,))
+                    # convert the magnitude in ADU using the zeropoint
+                    # magnitude.
+                    # Note that the zeropoint magnitude is define as 30,
+                    # so did not care of the exact value.
+                    # simply needed to draw random magnitudes.
+                    # We could estimate the proper one for our telescopes
                     maglist.append(mag[0])
-                    # trans_count.append(np.exp(0.921034 * (magzp - mag[0])))
                 else:
                     mag = np.array([magnitude[j]])
                     maglist.append(mag[0])
-                    # trans_count.append(np.exp(0.921034 * (magzp - mag[0])))
                 amp1 = np.exp(0.921034 * (magzp - mag))
+
                 # Apply Poisson Noise to simulated object
-                noisy_object1 = cpsf1 * amp1#np.random.poisson(cpsf1 * amp1)
-                noisy_object1[noisy_object1<0] = 0
+                noisy_object1 = cpsf1 * amp1
+                # np.random.poisson(cpsf1 * amp1)
+                noisy_object1[noisy_object1 < 0] = 0
                 noisy_object1 = np.random.poisson(noisy_object1)
                 noisy_object1 = noisy_object1 / gain
-                
+
                 ima1[iposrange] += noisy_object1
-                count = np.sum(noisy_object1) 
-            
+                count = np.sum(noisy_object1)
                 trans_count.append(count)
-                # print('amp1=', amp1)
-                # print('count=',count)
-                # print('ima1 = ', np.sum(ima1[iposrange]))
+
             hdusi1[0].data = ima1
-            # Write new fits file
+            #  Write new fits file
             hdusi1.writeto(newfile, overwrite=True)
 
             hdusi1.close()
             hdusp1.close()
-            counter+=1
+            counter += 1
     xypos = np.array(trans_pix)
     wcspos = np.array(trans_wcs)
     idx = np.arange(len(xypos))
-
     table = Table(
         [
             idx,
@@ -202,4 +213,3 @@ def sim(datapath, filenames, Ntrans=50, size=48,
         overwrite=True,
     )
     return table
-
