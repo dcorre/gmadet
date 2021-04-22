a470db5a2b5d219fbefa8d54675ceb0ef5d4df75
David Corre <david.corre@iap.fr>
2021-04-22 12:45:50 +0200

    numpy>=1.20.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-        'numpy',
+        'numpy>=1.20.0',
         'astropy',
         'matplotlib',
         'pandas',
