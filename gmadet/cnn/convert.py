68eaca737173cb70e37f3fdbd3f9d71622896375
dturpin-svom <dturpin-astro@hotmail.com>
2021-04-22 09:38:02 +0200

    adding an argument frac_true allowing to control the fraction of true/false one wants in the training set

--- gmadet/cnn/convert.py
+++ gmadet/cnn/convert.py
@@ -39,9 +39,10 @@ import numpy as np
 from astropy.io import fits
 import argparse
 from gmadet.utils import rm_p, mkdir_p
+from math import floor
 
 
-def convert(path_datacube, cubename, path_cutouts):
+def convert(path_datacube, cubename, path_cutouts, frac_true):
     """Convert simulated data before starting training"""
 
     outdir = os.path.join(path_datacube, "datacube")
@@ -52,45 +53,65 @@ def convert(path_datacube, cubename, path_cutouts):
     falselist = glob.glob(os.path.join(path_cutouts, "false", "*.fits"))
     # output cube name
     npz_name = "%s.npz" % cubename
+    Ncand_true = len(truelist)
+    Ncand_false = len(falselist)
+    if Ncand_true > Ncand_false:
+        Ncand_true_max = floor(2*Ncand_false*frac_true)
+        Ncand_false_max = floor(2*Ncand_false*(1-frac_true))
+    elif Ncand_true <= Ncand_false:
+        Ncand_true_max = floor(2*Ncand_true*frac_true)
+        Ncand_false_max = floor(2*Ncand_true*(1-frac_true))
 
-    Ncand = len(truelist) + len(falselist)
+    Ncand_tot = len(truelist) + len(falselist)
+    Ncand = Ncand_true_max + Ncand_false_max
     cube = []  # np.zeros((Ncand, 64, 64))
     labels = []
     mags = []
     errmags = []
     filters = []
+    counter_true = 0
     for cand in truelist:
-        hdus = fits.open(cand, memmap=False)
-        head = hdus[0].header
-        # Exclude cases too close to the edge
-        # Meaning they are located at less than the defined size
-        # of the small images
-        if head["EDGE"] == "False":
-            labels += [1]
-            mags += [head["MAG"]]
-            errmags += [head["MAGERR"]]
-            filters += [head["FILTER"]]
-            cube.append(hdus[0].data)
-        hdus.close()
+        if counter_true<Ncand_true_max:
+            hdus = fits.open(cand, memmap=False)
+            head = hdus[0].header
+            # Exclude cases too close to the edge
+            # Meaning they are located at less than the defined size
+            # of the small images
+            if head["EDGE"] == "False":
+                labels += [1]
+                mags += [head["MAG"]]
+                errmags += [head["MAGERR"]]
+                filters += [head["FILTER"]]
+                cube.append(hdus[0].data)
+            hdus.close()
+        else:
+            break
+        counter_true = counter_true+1
 
+    counter_false = 0
     for cand in falselist:
-        hdus = fits.open(cand, memmap=False)
-        head = hdus[0].header
-        # if hdus[0].data.shape != (64, 64):
-        #    print ('skip %s as its shape is not (64,64): (%d,%d)'
-        #           % (cand, hdus[0].data.shape[0], hdus[0].data.shape[1]))
-        #    continue
-        # Exclude cases too close to the edge
-        # Meaning they are located at less than the defined size
-        # of the small images
-        if head["EDGE"] == "False":
-            labels += [0]
-            mags += [head["MAG"]]
-            errmags += [head["MAGERR"]]
-            filters += [head["FILTER"]]
-            cube.append(hdus[0].data)
-        hdus.close()
+        if counter_false<Ncand_false_max:
+            hdus = fits.open(cand, memmap=False)
+            head = hdus[0].header
+            # if hdus[0].data.shape != (64, 64):
+            #    print ('skip %s as its shape is not (64,64): (%d,%d)'
+            #           % (cand, hdus[0].data.shape[0], hdus[0].data.shape[1]))
+                            # Exclude cases too close to the edge
+            # Meaning they are located at less than the defined size
+            # of the small images
+            if head["EDGE"] == "False":
+                labels += [0]
+                mags += [head["MAG"]]
+                errmags += [head["MAGERR"]]
+                filters += [head["FILTER"]]
+                cube.append(hdus[0].data)
+            hdus.close()
+        else:
+            break
+        counter_false = counter_false+1
 
+    print("The datacube contains "+str(Ncand)+" candidates with Ntrue = "+\
+    str(counter_true)+" and Nfalse = "+str(counter_false))
     print("Converting and reshaping arrays ...")
     # Convert lists to B.I.P. NumPy arrays
     # Check whether all candidates has 64x64 pixels
